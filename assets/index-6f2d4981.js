49c0f1fbf3909c6b7d6a976688fa1e214606d4bf
alexeynikinitin <alexeynikinitin@users.noreply.github.com>
2023-04-16 19:03:36 +0000

    deploy: 348fc24945a310562ffff7aea8eca0b4d167e1dd

--- assets/index-6f2d4981.js
+++
@@ -1,1 +0,0 @@
-import{a as o,j as n}from"./index-be441799.js";const t=()=>o("div",{className:"notFound",children:[n("h1",{className:"notFound__title",children:"Страница находится в разработке"}),n("img",{src:"/not-found/dev-page.png",alt:"img"})]});export{t as default};
--- assets/index-bbaa74f3.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./index-be441799.js";const e=()=>o("div",{children:"Login"});export{e as default};
